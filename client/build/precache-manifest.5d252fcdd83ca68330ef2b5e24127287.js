35bc872286d675c7558556fb051a7af43808c393
thomas <xsv24@hotmail.com>
2020-04-19 23:55:24 +0100

    attempt to fix heroku commit build folder

---
+++ client/build/precache-manifest.5d252fcdd83ca68330ef2b5e24127287.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9feb61ab04e17bbff899a5bbc526f185",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f75b1d8e19090624eb82",
+    "url": "/static/css/main.c4ba2314.chunk.css"
+  },
+  {
+    "revision": "838fa7e0c672c898134c",
+    "url": "/static/js/2.e89fa772.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e89fa772.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f75b1d8e19090624eb82",
+    "url": "/static/js/main.37f3d01f.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  }
+]);
