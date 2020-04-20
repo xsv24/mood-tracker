90656949d88f176ec50e254b3c7722cd7ea083db
thomas <xsv24@hotmail.com>
2020-04-20 06:09:12 +0100

    disgaurd client env var

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 export default process.env.NODE_ENV === 'production' ? {
-    api: `${process.env.CLIENT}/api`
+    api: `/api`
 } : {
     api: `http://localhost:${process.env.PORT || 3001}/api`
 }
